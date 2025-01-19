05658a0fc150181355d77614473e7792fae0f2c5
ilove <ilove@192.168.219.104>
2025-01-19 18:27:55 +0900

    연습파일 2025 01 19

---
+++ src/main/java/com/example/demo/DemoModel.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class DemoModel {
+
+	private String id;
+	
+	private String message;
+	
+
+}
